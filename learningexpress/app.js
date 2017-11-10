7c9e005c9dc24fe4c65d25d5237000ca84442064
ten-taken <Ten-Taken@users.noreply.github.com>
2017-11-10 14:55:27 -0600

    Learning Express - Route Parameters

--- learningexpress/app.js
+++ learningexpress/app.js
@@ -24,7 +24,18 @@ var app = express();
 */
 
 app.get('/', function (req,res) {
-	res.send("This is the homepage"); //Content type not neaded, Express handles it
+	res.send("This is the homepage"); //Content type not needed, Express handles it
+});
+
+/*Route Parameters with Express
+**
+	Often used with db queries for fetching dynamic data.
+	Parameters are args after : in path. 
+	Params are accessible via the request object.
+*/
+//Example of routing on a social network profile
+app.get('/profile/:id', function(req,res){
+	res.send('You requested the profile of id: '+req.params.id);  //param identifier can be anything
 });
 
 //Listening on port
