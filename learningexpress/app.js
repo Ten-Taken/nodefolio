890c604bdbc8cba4325a206c1626309f669d7ed3
ten-taken <Ten-Taken@users.noreply.github.com>
2017-11-11 12:53:10 -0600

    Learning Express -EJS Templating

--- learningexpress/app.js
+++ learningexpress/app.js
@@ -11,6 +11,8 @@ var express = require('express'); //Import module
 //The express module returns a function to store the entire object
 var app = express();
 
+//Setting a view engine in Express
+app.set('view engine', 'ejs');
 
 /*Networking with express
 **
@@ -44,13 +46,34 @@ app.get('/profile/:id', function(req,res){
 	Response object's .sendFile method is used to route html views. 
 	Again, Content-Type does not need to be declared with Express.
 
+*/
+app.get('/home', function (req,res) {
+	res.sendFile(__dirname+'/index.html');
+});
+
+/*
 	A templating engine allows embedding (injection) of data and structure
 	into the template before sending a rendered view.  Doing this with
 	EJS.  Reference http://www.embeddedjs.com/
+
+	By default, express looks in the /views directory
+	EJS template files use extension .ejs
+
+	When rendering a view, you use the .render method instead of sendFile.
+	The path is not needed, as default behavior is to check the views directory
 */
-app.get('/template', function (req,res) {
-	res.sendFile(__dirname+'/template.html');
+app.get('/template/:name', function (req,res) {
+	/*
+		Injecting data into a view. The second param is an object for the data.
+		The key of the kv pair becomes the accessor in the template.
+		See template.ejs
+	*/
+
+	//Additional data (often from db query)
+	var info = {age: 110, food: 'Sushi', language: 'English'};
+	res.render('template', {name: req.params.name, info: info});
 });
+
 
 //Listening on port
 app.listen(3000);
