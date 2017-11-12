a6266d777f9f36d7051910130da6419fc5aee441
ten-taken <Ten-Taken@users.noreply.github.com>
2017-11-12 17:01:13 -0600

    Site - Localized Bootstrap 4.0 beta

--- controllers/routeController.js
+++ controllers/routeController.js
@@ -5,5 +5,32 @@
 
 module.exports = function(app){
 
+	// Request Logging for server
+		app.use(function(req, res, next) {
 
-};
+		// Store remote address of request origin
+			var ip = req.headers['x-forwarded-for'] || 
+     			req.connection.remoteAddress || 
+     			req.socket.remoteAddress ||
+     			(req.connection.socket ? req.connection.socket.remoteAddress : null);
+
+		// log each request to the console
+			console.log(ip+' made a ',req.method+' request', ' via - '+req.url);
+
+		// continue doing what we were doing and go to the route
+			next(); 
+		});
+
+
+	// Index routing - (Chaining verbs)
+		var index = ['/', '/home']; //Array stores multiple valid paths for this route
+		app.route(index)
+			.get(function(req, res){
+				res.render('index');
+			})
+			.post(function(req, res){
+				res.send('That type of request is shunned in these parts.');
+			});
+
+
+}
--- nodefolio.js
+++ nodefolio.js
@@ -3,23 +3,49 @@
 	Welcome to Nodefolio. This is the entry point for the
 	server-side-rendering Express.js application used to power my 
 	web portfolio.
+
+	TO DO:
+		1. Handle XSS and CSRF vulnerabilities.
+		2. Set up Certificate Authority (Let's Encrypt)
+		3. Pull Bootstrap into local environment (nix CDN) **DONE**
+		4. Wireframe
+			1. Index
+			2. About
+			3. Dev Blog
+			4. Open Badges
+			5. Project Showcase
+			6. Hire me
+		5. Template Partials
+		6. Decide on data model, integrate relational or document db
+		7. Integrate Nodemailer
 */
 
 // Import modules.
 var express = require('express'); 
-var routeController = require('.controllers/routeController.js');
+var routeController = require('./controllers/routeController.js');
+
 
 // Define variables.
 var app = express();
 
+
 // Set template engine.
 app.set('view engine', 'ejs');
+
 
 // Static file routing middleware.
 app.use(express.static('./public'));
 
+// Static routing for localized Bootstrap
+app.use('/bootstrap/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); 
+app.use('/jquery/js', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/bootstrap/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/popper/js', express.static(__dirname + '/node_modules/popper.js/dist'));
+
+
 // Fire controllers
 routeController(app);
+
 
 // Server port for incoming requests
 app.listen(3000);
