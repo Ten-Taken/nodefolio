453eec8db7c6191fb295b84dd70cfe0ef14aecca
ten-taken <Ten-Taken@users.noreply.github.com>
2017-11-30 11:10:20 -0600

    Initial data handoff logic for blog view

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -11,9 +11,15 @@ module.exports = function(app){
 	var Post = app.get('connection').models.Post;
 	var Category = app.get('connection').models.Category;
 
+
+/* ====COMMENT SEPARATION FROM EXAMPLES =====*/
+
+
+
 //Manipulating records
 
-	//Inserting Category records on launch
+	//Inserting records in bulk
+	/*
 	Category.bulkCreate([
 	  { category: 'Development'},
 	  { category: 'Animals'},
@@ -22,10 +28,11 @@ module.exports = function(app){
 	  { category: 'Personal'},
 	  { category: 'Opinion'}
 	]);
+	*/
 
 	//Inserting a record ( .create() = immediate persistance.  .build() = is not stored until calling .save() )
 		//both .create() and .save() are async
-	
+	/*
 	Post.create({
 		category: 	"Development",
 		title: 		"Provisioning an Arch linux server",
@@ -46,6 +53,7 @@ module.exports = function(app){
 		author: 	"Gregory Wolfe", 
 		body: 		"Reintroduction efforts have been dwindling..."
 	});			
+	*/
 
 	//Retrieving a record
 	/*
@@ -57,13 +65,57 @@ module.exports = function(app){
 	//.then(function(error){
 	//	console.log(error); //Standard error handling promise
 	//});
+			
+			
+	//Func Def Promise  - Query category column of Category table (returns an array of objects)
+	/*
+	var categoryList = function(){
+
+		Category.findAll({attributes: ['category'], raw: true})
+			
+			.then(function (Category) {
+		            
+		            console.log(Category);
+		            //return Category;
+		         
+	    		})
+
+			.catch(function (error) {
+	            
+	            	console.log(error.message);
+	         		//return error.message;
+	        	});
+    	}
+
+	categoryList();
+    	*/
+
+/* ====COMMENT SEPARATION FROM EXAMPLES =====*/
 
 
 	// Blog Page view
 	app.route('/blog')
 		.get(function (req, res) {
-		res.render('blog');
+
+			Category.findAll({attributes: ['category'], raw: true})
+				
+				.then(function (Category) {
+			            
+			            var categoryList = Category;
+			            console.log(categoryList);
+
+			            res.render('blog',{categories: categoryList});
+			         
+		    		})
+
+				.catch(function (error) {
+		            
+		            	console.log(error.message);
+		         		//return error.message;
+		        	});
+
 		});
+
 
 
 }
